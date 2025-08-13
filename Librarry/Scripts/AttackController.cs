b11cba97d6c8ee9c209801603db3c11b1d2d36bb
Artur <arturgamedev007@gmail.com>
2025-08-13 19:05:46 +0500

    create

---
+++ Librarry/Scripts/AttackController.cs
@@ -0,0 +1,7 @@
+﻿namespace Assets.Librarry.Scripts
+{
+	public class AttackController
+	{
+		
+	}
+}
--- Librarry/Scripts/DetectCollision.cs
+++ Librarry/Scripts/DetectCollision.cs
@@ -1,12 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class DetectCollision : MonoBehaviour
+public class DetectCollision :MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
-        Destroy(other.gameObject);
+        Destroy(other.gameObject)
     }
 }
